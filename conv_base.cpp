e2a575b07cfed9e27c04c68e825c9f08a1d4eb9e
sfelissia <44405208+sfelissia@users.noreply.github.com>
2023-06-23 19:45:27 -0300

    Add files via upload

---
+++ conv_base.cpp
@@ -0,0 +1,35 @@
+/* Programa: Conversi?n por el m?todo de las divisiones sucesivas por la base*/
+
+#include <conio.h>
+#include <stdio.h>
+#include "stdlib.h"
+
+
+
+int main()
+{
+	int cociente, resto, num, base, numero;
+	char cadena[40];
+	
+	printf( "\n   Introduzca numero a convertir (entero): " );
+	scanf( "%d", &numero );
+	printf( "\n   Introduzca base (entero): " );
+	scanf( "%d", &base );
+	printf( "\n   " );
+	num=numero;
+	
+	while (cociente >0){
+		cociente=num/base;
+		resto=num%base;
+		printf("%d ",resto);
+		num=cociente;
+	}    
+	printf( "\n\n  " );
+	
+	
+	
+	printf( "\n   " );
+	
+	
+	return 0;
+}
